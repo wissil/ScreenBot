2512fcadb5e18dbeeb681b0a05628d68ef36a4f3
Milan Cop <milan.cop@infodyn.eu>
2018-12-07 15:09:49 +0100

    added ValueBettingBot to RebelService

--- main/src/main/java/com/util/ai/screenbot/main/RebelService.java
+++ main/src/main/java/com/util/ai/screenbot/main/RebelService.java
@@ -3,16 +3,24 @@ package com.util.ai.screenbot.main;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class RebelService {
-	
-	private static final Logger log = LoggerFactory.getLogger(RebelService.class);
+import com.google.inject.Inject;
+import com.util.ai.screenbot.input.logic.ValueBettingBot;
 
-	public void run() throws InterruptedException {
-		log.info("Service started successfully!");
-		
-		while (true) {
-			Thread.sleep(1000);
-			log.debug("Running ...");
-		}
-	}
+public class RebelService {
+
+    @Inject
+    private ValueBettingBot valueBettingBot;
+
+    private static final Logger log = LoggerFactory.getLogger(RebelService.class);
+
+    public void run() throws InterruptedException {
+        log.info("Service started successfully!");
+
+        valueBettingBot.switchToValueBetting();
+
+        while (true) {
+            Thread.sleep(1000);
+            log.debug("Running ...");
+        }
+    }
 }
