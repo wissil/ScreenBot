9a5bf080a8a27dd6060781a1c5b7d33beb808501
Filip Kozjak <filip.kozjak@asseco-see.hr>
2018-12-07 13:52:31 +0100

    implement main function

--- main/src/main/java/com/util/ai/screenbot/main/App.java
+++ main/src/main/java/com/util/ai/screenbot/main/App.java
@@ -4,35 +4,48 @@ import org.bytedeco.javacpp.tesseract.TessBaseAPI;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.util.ai.screenbot.input.config.InputHandlerModule;
+import com.util.ai.screenbot.output.config.OutputHandlerModule;
 import com.util.ai.screenbot.output.ocr.TesseractAPI;
+import com.util.ai.screenbot.support.config.SupportModule;
 
-/**
- * Hello world!
- *
- */
+
 public class App {
 	
-	private static final Logger logger = LoggerFactory.getLogger(App.class);
+	private static final Logger log = LoggerFactory.getLogger(App.class);
 	
 	private static final String TESSDATA_PATH = "../output-handler/tessdata";
 	
 	private static final String LANGUAGE = "eng";
 	
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException {    	
+    		log.info("Creating Guice injector ...");
+    		final Injector injector = Guice.createInjector(
+    				new SupportModule(), 
+    				new InputHandlerModule(),
+    				new OutputHandlerModule());
+    	
+    		log.info("Creating tesseract ...");
     		final TessBaseAPI tesseract = TesseractAPI.getTesseract(TESSDATA_PATH, LANGUAGE);
     	
+    		// register shutdown hook to destroy the tesseract
+    		log.info("Adding shutdown hook ...");
     		Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
 				
 				@Override
 				public void run() {
+					log.info("Destroying tesseract ...");
 					TesseractAPI.destroyTesseract(tesseract);
+					
+					log.info("Service stopped!");
 				}
 			}));
     		
-    		while (true) {
-    			Thread.sleep(1000);
-    			logger.debug("Running ... still ...");
-//    			System.out.println("Running ...");
-    		}
+    		final RebelService rebel = injector.getInstance(RebelService.class);
+    		
+    		log.info("Starting the service ...");
+    		rebel.run();
     }
 }
---
+++ main/src/main/java/com/util/ai/screenbot/main/RebelService.java
@@ -0,0 +1,18 @@
+package com.util.ai.screenbot.main;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RebelService {
+	
+	private static final Logger log = LoggerFactory.getLogger(RebelService.class);
+
+	public void run() throws InterruptedException {
+		log.info("Service started successfully!");
+		
+		while (true) {
+			Thread.sleep(1000);
+			log.debug("Running ...");
+		}
+	}
+}
