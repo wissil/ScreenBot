6b113fffa5e44566f9a3fc9b74aa6f142f9a6ce5
Filip Kozjak <filip.kozjak@asseco-see.hr>
2018-12-12 13:30:35 +0100

    clean ocr tests

--- output-handler/src/test/java/com/util/ai/screenbot/output/tests/ocr/OcrTest.java
+++ output-handler/src/test/java/com/util/ai/screenbot/output/tests/ocr/OcrTest.java
@@ -1,9 +1,6 @@
 package com.util.ai.screenbot.output.tests.ocr;
 
-import java.awt.Color;
-import java.awt.color.ColorSpace;
 import java.awt.image.BufferedImage;
-import java.awt.image.ColorConvertOp;
 import java.io.File;
 import java.io.IOException;
 
@@ -44,70 +41,30 @@ public class OcrTest extends OutputHandlerTestBase {
 	@Test
 	@TimedExecution
 	public void ocrFromImagePath_SingleFile_Test() throws IOException {
-		final BufferedImage inputFile = ImageIO.read(new File("./external/res/jakoKvalitetno-kliknuto.png"));
-		final BufferedImage bw = 
-				new BufferedImage(inputFile.getWidth(), inputFile.getHeight(), BufferedImage.TYPE_INT_ARGB);
-
-		ColorConvertOp op = 
-				new ColorConvertOp(ColorSpace.getInstance(ColorSpace.CS_GRAY), null);
-		BufferedImage dest = op.filter(inputFile, null);
-		ImageIO.write(dest, "png", new File("./external/output/jakoKvalitetno-kliknutoBW1.png"));
-
-		final String textual1 = ocr.doOcr(api, inputFile);
-
-		final Color white = new Color(255, 255, 255);
-		final Color black = new Color(0, 0, 0);
-
-		for (int x = 0; x < inputFile.getWidth(); x++) {
-			for (int y = 0; y < inputFile.getHeight(); y++) {
-				int rgba = inputFile.getRGB(x, y);
-				Color col = new Color(rgba, true);
-
-				if (isWhite(col)) {
-					inputFile.setRGB(x, y, black.getRGB());
-				} else {
-					inputFile.setRGB(x, y, white.getRGB());
-				}
-				
-//				inputFile.setRGB(x, y, myCol.getRGB());
-
-				//				col = new Color(255 - col.getRed(), 255 - col.getGreen(),
-				//						255 - col.getBlue());
-				//				inputFile.setRGB(x, y, col.getRGB());
-			}
+		final BufferedImage inputFile = 
+				ImageIO.read(new File("./external/res/jakoKvalitetno-kliknuto.png"));
+		final String textual = ocr.doOcr(api, inputFile);
+		System.out.println(textual);
+	}
+		
+		@Test
+		@TimedExecution
+		public void ocrFromBufferedImage_SingleFile_Test() throws IOException {
+			final String textual = ocr.doOcr(api, bImage);
+			System.out.println(textual);
 		}
-
-		ImageIO.write(inputFile, "png", new File("./external/output/kliknuto8.png"));
-
-
-		final String textual2 = ocr.doOcr(api, inputFile);
-		System.out.println(textual1);
-		System.out.println(textual2);
-	}
-
-	private static boolean isWhite(Color col) {
-		final int tresh = 210;
-		return col.getGreen() >= tresh || col.getBlue() >= tresh || col.getRed() >= tresh;
-	}
-	//	
-	//	@Test
-	//	@TimedExecution
-	//	public void ocrFromBufferedImage_SingleFile_Test() throws IOException {
-	//		final String textual = ocr.doOcr(api, bImage);
-	//		System.out.println(textual);
-	//	}
-	//	
-	//	@Test
-	//	@Ignore
-	//	public void interpretMultipleImagesTest() throws IOException {
-	//		String imagePath = "./external/res/t1.png";
-	//		String textual = ocr.doOcr(api, imagePath);
-	//		System.out.println(textual);
-	//		
-	//		imagePath = "./external/res/test2.jpg";
-	//		textual = ocr.doOcr(api, imagePath);
-	//		System.out.println(textual);
-	//	}
+		
+		@Test
+		@Ignore
+		public void interpretMultipleImagesTest() throws IOException {
+			String imagePath = "./external/res/t1.png";
+			String textual = ocr.doOcr(api, imagePath);
+			System.out.println(textual);
+			
+			imagePath = "./external/res/test2.jpg";
+			textual = ocr.doOcr(api, imagePath);
+			System.out.println(textual);
+		}
 
 	@AfterClass
 	public static void after() {
