81429022e7f45f83e40f8d98842d91621a719471
Filip Kozjak <filip.kozjak@asseco-see.hr>
2019-02-07 16:36:54 +0100

    add min stake regex

--- output-handler/src/main/java/com/util/ai/screenbot/output/parsing/VBBookmakerMinStakeElementParser.java
+++ output-handler/src/main/java/com/util/ai/screenbot/output/parsing/VBBookmakerMinStakeElementParser.java
@@ -1,20 +1,27 @@
 package com.util.ai.screenbot.output.parsing;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import com.util.ai.screenbot.output.elements.VBBookmakerMinStakeElement;
 import com.util.ai.screenbot.output.parsing.exceptions.ScreenElementParseException;
 import com.util.ai.screenbot.support.numbers.CustomNumberFormat;
 
 public class VBBookmakerMinStakeElementParser implements VBScreenElementParser<VBBookmakerMinStakeElement> {
 
+	private static final Pattern PATTERN_MIN_STAKE = Pattern.compile(
+			"(?:.*)(\\d+(\\.\\d)?\\d*)"); 
+	
 	@Override
 	public VBBookmakerMinStakeElement parse(String input) throws ScreenElementParseException {
-		try {
-			final double stake = CustomNumberFormat.parseDouble(input);
-			return new VBBookmakerMinStakeElement(stake);
-		} catch (Exception e) {
+		final Matcher matcher = PATTERN_MIN_STAKE.matcher(input.trim());
+		if (!matcher.matches()) {
 			throw new ScreenElementParseException(
-					String.format("Couldn't parse input: %s to Double.", input));
+					String.format("Input %s doesn't correspond to the MIN_STAKE pattern.", input.trim()));
 		}
+		
+		final double stake = CustomNumberFormat.parseDouble(matcher.group(1));
+		return new VBBookmakerMinStakeElement(stake);
 	}
 
 }
---
+++ output-handler/src/test/java/com/util/ai/screenbot/output/tests/ocr/MinStakeOcrTest.java
@@ -0,0 +1,59 @@
+package com.util.ai.screenbot.output.tests.ocr;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.google.inject.Inject;
+import com.util.ai.screenbot.output.elements.ocr.conf.general.VBBetInfoOcrConf;
+import com.util.ai.screenbot.output.ocr.OCR;
+import com.util.ai.screenbot.output.ocr.OcrImageProcessingConf;
+import com.util.ai.screenbot.output.ocr.OcrImageProcessor;
+import com.util.ai.screenbot.output.ocr.OcrReadMode;
+import com.util.ai.screenbot.output.tests.config.OutputHandlerTestBase;
+import com.util.ai.screenbot.support.numbers.CustomNumberFormat;
+
+public class MinStakeOcrTest extends OutputHandlerTestBase {
+
+	@Inject
+	private OCR ocr;
+
+	@Inject
+	private OcrImageProcessor imageProcessor;
+	
+	private static OcrImageProcessingConf conf;
+	
+	@BeforeClass
+	public static void setup() {
+		conf = new VBBetInfoOcrConf();
+	}
+
+	@Test
+	public void ocrTest() throws Exception {
+		final File root = new File("./external/minStake/");
+
+		for (File f : root.listFiles()) {
+			BufferedImage image = ImageIO.read(f);
+			image = imageProcessor.process(image, conf);
+
+			String result = ocr.doOcr(image, OcrReadMode.DIGITS);
+			double d = CustomNumberFormat.parseDouble(result);
+			System.out.println(f.getName() + ": " + result + ", d: " + d);
+		}
+	}
+
+	@Test
+	public void betInfoImageTest() throws IOException {
+		final File f = new File("./external/minStake/latest.png");
+		final BufferedImage in = ImageIO.read(f);
+		final BufferedImage out = imageProcessor.process(in, conf);
+		ImageIO.write(out, "png", new File("./external/out/" + f.getName()));
+		String result = ocr.doOcr(out, conf.OCR_READ_MODE());
+		System.out.println(result);
+	}
+}
