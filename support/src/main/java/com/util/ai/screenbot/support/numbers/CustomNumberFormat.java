8fcb3d4dd3f5a5ccf91bd38d9fe105603279dfc8
Filip Kozjak <filip.kozjak@asseco-see.hr>
2019-02-12 11:22:05 +0100

    enhance parsing doubles

--- support/src/main/java/com/util/ai/screenbot/support/numbers/CustomNumberFormat.java
+++ support/src/main/java/com/util/ai/screenbot/support/numbers/CustomNumberFormat.java
@@ -1,12 +1,51 @@
 package com.util.ai.screenbot.support.numbers;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class CustomNumberFormat {
+	
+	private static final String ANY_WHITESPACE_REGEX = "\\s+";
+	
+	private static final String EMPTY_STRING = "";
+	
+	private static final char DOT = '.';
+	
+	private static final char COMMA = ',';
+	
+	private static final Set<Character> SEPARATORS = new HashSet<>(Arrays.asList(DOT, COMMA));
 
 	public static double parseDouble(String s) {
-		int startIdx = 0;
-		for (; startIdx < s.length() && !Character.isDigit(s.charAt(startIdx)); startIdx++)
-			;
-
-		return Double.parseDouble(s.substring(startIdx).replace(',', '.').replaceAll("\\s", ""));
+		// replace all commas with the dots
+		// remove all white-spaces
+		s = s.replace(COMMA, DOT).replaceAll(ANY_WHITESPACE_REGEX, EMPTY_STRING);
+		
+		// ignore all whitespace occurrences before the number starts
+		int startIdx = -1;
+		while (isSeparator(s.charAt(++startIdx)));
+		s = s.substring(startIdx);
+		
+		// get positions of separators
+		final Map<Integer, Character> separatorPositions = new HashMap<>();		
+		for (int i=startIdx, n=s.length(); i<n; i++) {
+			final char c = s.charAt(i);
+			if (isSeparator(c)) separatorPositions.put(i, c);
+		}
+		
+		// if there are exactly two, number represents a number greater than 1,000
+		// remove the first dot (previously replaced the comma)		
+		if (separatorPositions.size() == 2) {
+			s = s.replaceFirst("\\.", EMPTY_STRING);
+		}
+				
+		// if there were more than 2 separators left, parseDouble will throw
+		return Double.parseDouble(s);
+	}
+	
+	private static boolean isSeparator(char c) {
+		return SEPARATORS.contains(c);
 	}
 }
--- support/src/test/java/com/util/ai/screenbot/support/tests/numbers/CustomNumberFormatTest.java
+++ support/src/test/java/com/util/ai/screenbot/support/tests/numbers/CustomNumberFormatTest.java
@@ -26,4 +26,43 @@ public class CustomNumberFormatTest extends SupportTestBase {
 		assertEquals(expected, CustomNumberFormat.parseDouble(s3), PRECISION);
 		assertEquals(expected, CustomNumberFormat.parseDouble(s4), PRECISION);
 	}
+	
+	@Test
+	public void numberFormatAboveTenTest() {		
+		final String s1 = ". 12.20";
+		final String s2 = "12.20";
+		final String s3 = "12,20";
+		final String s4 = " , 12.2";
+		
+		final double expected = 12.2;
+		
+		assertEquals(expected, CustomNumberFormat.parseDouble(s1), PRECISION);
+		assertEquals(expected, CustomNumberFormat.parseDouble(s2), PRECISION);
+		assertEquals(expected, CustomNumberFormat.parseDouble(s3), PRECISION);
+		assertEquals(expected, CustomNumberFormat.parseDouble(s4), PRECISION);
+	}
+	
+	@Test
+	public void numberFormatAboveThousandTest() {		
+		final String s1 = ". 1.200,0";
+		final String s2 = "1,200. 0";
+		
+		final double expected = 1200.0;
+						
+		assertEquals(expected, CustomNumberFormat.parseDouble(s1), PRECISION);
+		assertEquals(expected, CustomNumberFormat.parseDouble(s2), PRECISION);
+	}
+	
+	@Test
+	public void numberFormatAboveThousandWithDecimalsTest() {		
+		final String s1 = "1.200,30";
+		final String s2 = "1, 200. 30";
+		final String s3 = ". 1, 200. 30";
+		
+		final double expected = 1200.3;
+						
+		assertEquals(expected, CustomNumberFormat.parseDouble(s1), PRECISION);
+		assertEquals(expected, CustomNumberFormat.parseDouble(s2), PRECISION);
+		assertEquals(expected, CustomNumberFormat.parseDouble(s3), PRECISION);
+	}
 }
