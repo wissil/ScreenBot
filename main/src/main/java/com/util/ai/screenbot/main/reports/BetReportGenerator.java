7a5b1b096305fb56eb795f0788c9257c9ef502e8
Milan Cop <milan.nalim@gmail.com>
2019-02-15 23:25:05 +0100

    fix constants and reports

--- main/src/main/java/com/util/ai/screenbot/main/reports/BetReportGenerator.java
+++ main/src/main/java/com/util/ai/screenbot/main/reports/BetReportGenerator.java
@@ -1,18 +1,15 @@
 package com.util.ai.screenbot.main.reports;
 
-
 public class BetReportGenerator {
-	
+
 	private static final String REPORT_HEADER = "================================ BET REPORT =================================";
-	
-	public static String generateReport(
-			String bookmaker,
-			double value, double oddsLimit, double oddsActual,
-			double stake, double balance, double max, double min,
-			boolean shouldPlaceBet) {
+
+	public static String generateReport(String bookmaker, double value, double oddsLimit, double oddsActual,
+			double stake, double balance, double max, double min, boolean shouldPlaceBet) {
 		final int len = REPORT_HEADER.length();
 		final StringBuilder sb = new StringBuilder();
-		
+
+		sb.append(System.lineSeparator());
 		sb.append(REPORT_HEADER);
 		sb.append(System.lineSeparator());
 		sb.append(generateSingleParameter(sb, "Bookmaker", bookmaker, len));
@@ -29,42 +26,46 @@ public class BetReportGenerator {
 		sb.append(generateSingleParameter(sb, "Value", String.valueOf(value), len));
 		sb.append(generateSingleParameter(sb, "Should place bet", String.valueOf(shouldPlaceBet), len));
 		sb.append(generateFooter(sb, len));
-		
+
 		return sb.toString();
 	}
-	
-	private static StringBuilder generateSingleParameter(StringBuilder sb, String paramKey, String paramValue, int len) {
+
+	private static StringBuilder generateSingleParameter(StringBuilder sb, String paramKey, String paramValue,
+			int len) {
 		sb = new StringBuilder();
 		sb.append("| ");
 		sb.append(paramKey);
 		sb.append(": ");
 		sb.append(paramValue);
-		
-		for (int i=sb.length(); i<len-1; i++) sb.append(" ");
+
+		for (int i = sb.length(); i < len - 1; i++)
+			sb.append(" ");
 		sb.append("|");
 		sb.append(System.lineSeparator());
-		
+
 		return sb;
 	}
-	
+
 	private static StringBuilder generateEmptyLine(StringBuilder sb, int len) {
 		sb = new StringBuilder();
 		sb.append("| ");
-		
-		for (int i=sb.length(); i<len-1; i++) sb.append(" ");
+
+		for (int i = sb.length(); i < len - 1; i++)
+			sb.append(" ");
 		sb.append("|");
 		sb.append(System.lineSeparator());
-		
+
 		return sb;
 	}
-	
+
 	private static StringBuilder generateFooter(StringBuilder sb, int len) {
 		sb = new StringBuilder();
-		
-		for (int i=sb.length(); i<len; i++) sb.append("=");
+
+		for (int i = sb.length(); i < len; i++)
+			sb.append("=");
 		sb.append(System.lineSeparator());
-		
+
 		return sb;
 	}
-	
+
 }
