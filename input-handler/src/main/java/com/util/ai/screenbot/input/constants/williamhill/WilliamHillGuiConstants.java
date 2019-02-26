99e240ac27640de70fd60a2f9cfed366cb0488ff
Milan Cop <milan.cop@infodyn.eu>
2019-02-26 15:56:01 +0100

    introduced william hill

---
+++ input-handler/src/main/java/com/util/ai/screenbot/input/constants/williamhill/WilliamHillGuiConstants.java
@@ -0,0 +1,16 @@
+package com.util.ai.screenbot.input.constants.williamhill;
+
+public class WilliamHillGuiConstants {
+
+    private static final String WILLIAM_HILL_PREFIX = "williamhill/";
+
+    private static final String MIME_TYPE = ".png";
+
+    public static final String WILLIAM_HILL_BET_SLIP_LOADED = WILLIAM_HILL_PREFIX + "WilliamHill_BetSlip_Loaded" + MIME_TYPE;
+
+    public static final String WILLIAM_HILL_INPUT_STAKE_TEXT = WILLIAM_HILL_PREFIX + "WilliamHill_InputText" + MIME_TYPE;
+
+    public static final String WILLIAM_HILL_PLACE_BET = WILLIAM_HILL_PREFIX + "WilliamHill_PlaceBet" + MIME_TYPE;
+
+    public static final String WILLIAM_HILL_REMOVE_ALL = WILLIAM_HILL_PREFIX + "WilliamHill_ClearSlip" + MIME_TYPE;
+}
---
+++ input-handler/src/main/java/com/util/ai/screenbot/input/logic/williamhill/WilliamHillInputBot.java
@@ -0,0 +1,61 @@
+package com.util.ai.screenbot.input.logic.williamhill;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.util.ai.screenbot.input.constants.williamhill.WilliamHillGuiConstants;
+import com.util.ai.screenbot.input.exceptions.GuiElementNotFoundException;
+import com.util.ai.screenbot.input.exceptions.InvalidBetSlipException;
+import com.util.ai.screenbot.input.handlers.keyboard.KeyboardHandler;
+import com.util.ai.screenbot.input.handlers.mouse.MouseHandler;
+import com.util.ai.screenbot.input.handlers.screen.ScreenHandler;
+import com.util.ai.screenbot.input.logic.AbstractInputBot;
+import com.util.ai.screenbot.input.utils.SikuliUtils;
+
+public class WilliamHillInputBot extends AbstractInputBot {
+
+    protected static final Logger log = LoggerFactory.getLogger(WilliamHillInputBot.class);
+
+    protected WilliamHillInputBot(KeyboardHandler keyboardHandler, ScreenHandler screenHandler, MouseHandler mouseHandler) {
+        super(keyboardHandler, screenHandler, mouseHandler);
+
+    }
+
+    public void checkBettingSlip() throws InvalidBetSlipException {
+        log.debug("Waiting for the bet slip to load ...");
+
+        if (!SikuliUtils.waitForElement(WilliamHillGuiConstants.WILLIAM_HILL_BET_SLIP_LOADED, 5)) {
+            throw new InvalidBetSlipException("Bet slip header element is missing.");
+        }
+
+        log.debug("Bet slip successfully loaded!");
+    }
+
+    public void clickRemoveAll() throws InvalidBetSlipException {
+        log.debug("Clicking remove all button ...");
+
+        try {
+            SikuliUtils.clickOnElement(WilliamHillGuiConstants.WILLIAM_HILL_REMOVE_ALL);
+        } catch (GuiElementNotFoundException e) {
+            throw new InvalidBetSlipException("Remove all element is missing.", e);
+        }
+    }
+
+    public void clickBet() throws InvalidBetSlipException {
+        log.debug("Clicking the bet button ...");
+
+        try {
+            SikuliUtils.clickOnElement(WilliamHillGuiConstants.WILLIAM_HILL_PLACE_BET);
+        } catch (GuiElementNotFoundException e) {
+            throw new InvalidBetSlipException("Place bet element is missing.", e);
+        }
+    }
+
+    public void setBetStake(String stake) throws InvalidBetSlipException {
+        log.debug("Setting the bet stake ...");
+
+        if (SikuliUtils.writeToElement(WilliamHillGuiConstants.WILLIAM_HILL_INPUT_STAKE_TEXT, String.valueOf(stake)))
+            return;
+        throw new InvalidBetSlipException("Could not find input stake element.");
+    }
+}
