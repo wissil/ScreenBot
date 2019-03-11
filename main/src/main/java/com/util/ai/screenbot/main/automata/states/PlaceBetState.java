a8f2dcf9cba889e04812439a4f6959c586b687a1
Milan Cop <milan.nalim@gmail.com>
2019-03-11 14:59:37 +0100

    slow down due to Policy error

--- main/src/main/java/com/util/ai/screenbot/main/automata/states/PlaceBetState.java
+++ main/src/main/java/com/util/ai/screenbot/main/automata/states/PlaceBetState.java
@@ -63,7 +63,7 @@ public class PlaceBetState extends VBState {
 
 			log.debug("Betting slip successfully checked!");
 
-			Thread.sleep(200);
+			Thread.sleep(1000); // TODO increased for testing purposes. 200 ms was not preventing "Policy" bug
 			final VBBetInfoElement betInfo = out.readBetInfo(in.getOddsInputImage());
 			final VBBookmakerOddsElement bookmakerOdds = out.readBookmakerOdds(in.getBookmakerOddsImage(bookie),
 					bookie);
