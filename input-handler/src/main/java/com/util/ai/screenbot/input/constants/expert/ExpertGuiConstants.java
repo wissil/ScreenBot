70f21677d35a5a8d422e00be20872f04cfddb537
Milan Cop <milan.cop@infodyn.eu>
2019-03-13 15:49:22 +0100

    introduced expert

---
+++ input-handler/src/main/java/com/util/ai/screenbot/input/constants/expert/ExpertGuiConstants.java
@@ -0,0 +1,21 @@
+package com.util.ai.screenbot.input.constants.expert;
+
+public class ExpertGuiConstants {
+
+    private static final String EXPERT_PREFIX = "expert/";
+
+    private static final String MIME_TYPE = ".png";
+
+    public static final String EXPERT_INPUT_STAKE_TEXT = EXPERT_PREFIX + "Expert_Stake" + MIME_TYPE;
+
+    public static final String EXPERT_BALANCE = EXPERT_PREFIX + "Expert_Balance" + MIME_TYPE;
+
+    public static final String EXPERT_ODDS = EXPERT_PREFIX + "Expert_Odds" + MIME_TYPE;
+
+    public static final String EXPERT_BET_BUTTON_ACTIVE = EXPERT_PREFIX + "Expert_BetButton_Active" + MIME_TYPE;
+
+    public static final String EXPERT_BET_BUTTON_NOT_ACTIVE = EXPERT_PREFIX + "Expert_BetButton_NotActive" + MIME_TYPE;
+
+    public static final String EXPERT_REMOVE_ALL = EXPERT_PREFIX + "Expert_RemoveAll" + MIME_TYPE;
+
+}
---
+++ input-handler/src/main/java/com/util/ai/screenbot/input/logic/expert/ExpertInputBot.java
@@ -0,0 +1,83 @@
+package com.util.ai.screenbot.input.logic.expert;
+
+import java.awt.image.BufferedImage;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.util.ai.screenbot.input.constants.expert.ExpertGuiConstants;
+import com.util.ai.screenbot.input.exceptions.FatalVBException;
+import com.util.ai.screenbot.input.exceptions.GuiElementNotFoundException;
+import com.util.ai.screenbot.input.exceptions.InvalidBetSlipException;
+import com.util.ai.screenbot.input.handlers.keyboard.KeyboardHandler;
+import com.util.ai.screenbot.input.handlers.mouse.MouseHandler;
+import com.util.ai.screenbot.input.handlers.screen.ScreenHandler;
+import com.util.ai.screenbot.input.logic.AbstractInputBot;
+import com.util.ai.screenbot.input.logic.williamhill.WilliamHillInputBot;
+import com.util.ai.screenbot.input.utils.SikuliUtils;
+
+public class ExpertInputBot extends AbstractInputBot {
+
+    protected static final Logger log = LoggerFactory.getLogger(WilliamHillInputBot.class);
+
+    public ExpertInputBot(KeyboardHandler keyboardHandler, ScreenHandler screenHandler, MouseHandler mouseHandler) {
+        super(keyboardHandler, screenHandler, mouseHandler);
+
+    }
+
+    public void checkBettingSlip() throws InvalidBetSlipException {
+        log.debug("Waiting for the bet slip to load ...");
+
+        // TODO
+
+        log.debug("Bet slip successfully loaded!");
+    }
+
+    public void clickRemoveAll() throws InvalidBetSlipException {
+        log.debug("Clicking remove all button ...");
+
+        try {
+            SikuliUtils.clickOnElement(ExpertGuiConstants.EXPERT_REMOVE_ALL);
+        } catch (GuiElementNotFoundException e) {
+            throw new InvalidBetSlipException("Remove all element is missing.", e);
+        }
+    }
+
+    public void clickBet() throws InvalidBetSlipException {
+        log.debug("Clicking the bet button ...");
+
+        try {
+            SikuliUtils.clickOnElement(ExpertGuiConstants.EXPERT_BET_BUTTON_ACTIVE);
+        } catch (GuiElementNotFoundException e) {
+            throw new InvalidBetSlipException("Place bet element is missing.", e);
+        }
+    }
+
+    public void setBetStake(String stake) throws InvalidBetSlipException {
+        log.debug("Setting the bet stake ...");
+
+        if (SikuliUtils.writeToElement(ExpertGuiConstants.EXPERT_INPUT_STAKE_TEXT, String.valueOf(stake)))
+            return;
+        throw new InvalidBetSlipException("Could not find input stake element.");
+    }
+
+    public BufferedImage takeBalanceScreenshot() throws FatalVBException {
+        log.debug("Taking bookmaker balance screenshot ...");
+
+        try {
+            return SikuliUtils.getImageLeftToElement(ExpertGuiConstants.EXPERT_BALANCE, 70);
+        } catch (GuiElementNotFoundException e) {
+            throw new FatalVBException("Balance could not be read.");
+        }
+    }
+
+    public BufferedImage takeBookmakerOddsScreenshot() throws InvalidBetSlipException {
+        log.debug("Taking bookmaker odds screenshot ...");
+
+        try {
+            return SikuliUtils.getImageRigtToElement(ExpertGuiConstants.EXPERT_ODDS, 20);
+        } catch (GuiElementNotFoundException e) {
+            throw new InvalidBetSlipException("Odds input element is missing.", e);
+        }
+    }
+}
